00ca0d85eb7f1b604a3ddb7b2f286b2b3aaf5dd6
psajd <polpilik@gmail.com>
2023-12-08 12:35:10 +0300

    moved to BEM model

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function typeWriter(textElement, text, speed) {
     let i = 0;
-    const breakPoint = 'Backend developer from Saint-Petersburg, Russia.';
+    const breakPoint1 = 'Backend developer from ';
+    const breakPoint2 = 'Saint-Petersburg, Russia.';
     function type() {
         if (i < text.length) {
             textElement.innerHTML += text.charAt(i);
 
-            if (textElement.innerHTML.endsWith(breakPoint)) {
+            if (textElement.innerHTML.endsWith(breakPoint2)) {
                 textElement.innerHTML += '<br><br>';
+            }
+
+            if (textElement.innerHTML.endsWith(breakPoint1)) {
+                textElement.innerHTML += '<br>';
             }
 
             i++;
@@ -16,7 +21,7 @@ function typeWriter(textElement, text, speed) {
     type();
 }
 
-const aboutTextElement = document.querySelector('.about-text');
+const aboutTextElement = document.querySelector('.about__personal-text');
 
 const yourText = 'Backend developer from Saint-Petersburg, Russia. Java, Kotlin enjoyer.';
 
